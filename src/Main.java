122be74b9b683f62e017d56831e5fb920f1bebba
Michael <mihael.bazkor@gmail.com>
2024-06-21 10:31:20 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static Scanner scanner; // Note: Do not change this line.
+    public static void manageGrades() {}
+    public static void main(String[] args) throws IOException {
+        String path = args[0];
+        scanner = new Scanner(new File(path));
+        int numberOfTests = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 1; i <= numberOfTests; i++) {
+            System.out.println("Test number " + i + " starts.");
+            try {
+                manageGrades();
+            } catch(Exception e){
+                System.out.println("Exception " + e);
+            }
+            System.out.println("Test number " + i + " ended.");
+            System.out.println("-----------------------------------------------");
         }
+        System.out.println("All tests have ended.");
     }
 }
